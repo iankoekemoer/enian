b57d1a3491568e7e48ce5bcd9056211dc06ce62e
iankoekemoer <31848409+iankoekemoer@users.noreply.github.com>
2021-01-06 15:54:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+/** Hamburger Menu **/
+
+const hamburgerContainer = document.querySelector("#header__main-nav");
+const hamburger = document.querySelector(".header__main-nav--hamburger");
+const links = document.querySelectorAll(".header__main-nav--links li");
+
+hamburger.addEventListener("click", () => {
+  hamburgerContainer.classList.toggle("clicked");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
+
+/** Cursor **/
+
+var cursor = document.querySelector(".cursor");
+    var cursor2 = document.querySelector(".cursor2");
+    document.addEventListener("mousemove",function(e){
+      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    });
+
+/** What we do **/
+
+var testimonialItems = document.querySelectorAll(".item label");
+var timer;
+function cycleTestimonials(index) {
+   timer = setTimeout(function() {
+    var evt;
+    if (document.createEvent){
+      //If browser = IE, then polyfill
+      evt = document.createEvent('MouseEvent');
+      evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    } else {
+      //If Browser = modern, then create new MouseEvent
+      evt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            clientX: 20
+          });
+    }
+    var ele = "." + testimonialItems[index].className;
+    var ele2 = document.querySelector(ele)
+    ele2.dispatchEvent(evt);
+    index++; // Increment the index
+    if (index >= testimonialItems.length) {
+      index = 0; // Set it back to `0` when it reaches `3`
+    }
+    cycleTestimonials(index); // recursively call `cycleTestimonials()`
+    document.querySelector(".testimonials").addEventListener("click", function() {
+      clearTimeout(timer); //stop the carousel when someone clicks on the div
+    });
+  }, 2000); //adjust scroll speed in miliseconds
+}
+//run the function
+cycleTestimonials(0);
+
+
+/** what we do nav */
+
+console.clear();
+const elApp = document.querySelector("#app");
+
+// What we need...
+// Mouse position
+// Currently hovered item
+
+document.body.addEventListener("mousemove", (event) => {
+  console.log({
+    x: event.clientX,
+    y: event.clientY
+  });
+
+  elApp.style.setProperty("--x", event.clientX);
+  elApp.style.setProperty("--y", event.clientY);
+});
+
+// Replay animation by hiding & showing the element again
+let el = document.body;
+el.addEventListener("click", function (e) {
+  el.hidden = true;
+  requestAnimationFrame(() => {
+    el.hidden = false;
+  });
+});
+
+/** We redo websites **/
+
+let didScroll = false;
+let paralaxTitles = document.querySelectorAll('.paralax-title');
+
+const scrollInProgress = () => {
+	didScroll = true
+}
+
+const raf = () => {
+	if(didScroll) {
+		paralaxTitles.forEach((element, index) => {
+			element.style.transform = "translateX("+ window.scrollY / 10 + "%)"
+		})
+		didScroll = false;
+	}
+	requestAnimationFrame(raf);
+}
+
+
+requestAnimationFrame(raf);
+window.addEventListener('scroll', scrollInProgress)
+
+/* Emblem circular */
+var Emblem = {
+  init: function(el, str) {
+    var element = document.querySelector(el);
+    var text = str ? str : element.innerHTML;
+    element.innerHTML = '';
+    for (var i = 0; i < text.length; i++) {
+      var letter = text[i];
+      var span = document.createElement('span');
+      var node = document.createTextNode(letter);
+      var r = (360/text.length)*(i);
+      var x = (Math.PI/text.length).toFixed(0) * (i);
+      var y = (Math.PI/text.length).toFixed(0) * (i);
+      span.appendChild(node);
+      span.style.webkitTransform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+      span.style.transform = 'rotateZ('+r+'deg) translate3d('+x+'px,'+y+'px,0)';
+      element.appendChild(span);
+    }
+  }
+};
+
+Emblem.init('.emblem');
+
